d84774f013832595d86eaa4f38932abe6485cef6
nogy21 <nogy21@gmail.com>
2025-02-27 22:40:36 +0900

    ⚡️ SearchDialog 컴포넌트 fetchAllLectures 함수 캐싱 처리
    - 클로저를 활용하여 lecture data를 캐싱 처리 -> 불필요한 API 호출 회수 감소

--- src/SearchDialog.tsx
+++ src/SearchDialog.tsx
@@ -32,7 +32,7 @@ import {
 import { useScheduleContext } from './ScheduleContext.tsx';
 import { Lecture } from './types.ts';
 import { parseSchedule } from './utils.ts';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { DAY_LABELS } from './constants.ts';
 
 interface Props {
@@ -85,16 +85,26 @@ const PAGE_SIZE = 100;
 const fetchMajors = () => axios.get<Lecture[]>('/schedules-majors.json');
 const fetchLiberalArts = () => axios.get<Lecture[]>('/schedules-liberal-arts.json');
 
-// TODO: 이 코드를 개선해서 API 호출을 최소화 해보세요 + Promise.all이 현재 잘못 사용되고 있습니다. 같이 개선해주세요.
-const fetchAllLectures = async () =>
-  await Promise.all([
-    (console.log('API Call 1', performance.now()), fetchMajors()),
-    (console.log('API Call 2', performance.now()), fetchLiberalArts()),
-    (console.log('API Call 3', performance.now()), fetchMajors()),
-    (console.log('API Call 4', performance.now()), fetchLiberalArts()),
-    (console.log('API Call 5', performance.now()), fetchMajors()),
-    (console.log('API Call 6', performance.now()), fetchLiberalArts()),
-  ]);
+const fetchAllLectures = (() => {
+  let cache: AxiosResponse<Lecture[]>[] = [];
+
+  return async () => {
+    if (cache && cache.length > 0) {
+      return cache;
+    }
+
+    cache = await Promise.all([
+      (console.log('API Call 1', performance.now()), fetchMajors()),
+      (console.log('API Call 2', performance.now()), fetchLiberalArts()),
+      (console.log('API Call 3', performance.now()), fetchMajors()),
+      (console.log('API Call 4', performance.now()), fetchLiberalArts()),
+      (console.log('API Call 5', performance.now()), fetchMajors()),
+      (console.log('API Call 6', performance.now()), fetchLiberalArts()),
+    ]);
+
+    return cache;
+  };
+})();
 
 // TODO: 이 컴포넌트에서 불필요한 연산이 발생하지 않도록 다양한 방식으로 시도해주세요.
 const SearchDialog = ({ searchInfo, onClose }: Props) => {
